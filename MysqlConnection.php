26246b0836f27c55609b5e02e2745e148284dc38
ArthurSantosOliveira <pessoalarthursoliveira@gmail.com>
2023-05-03 15:13:13 -0300

    if tabelas
    caso o banco de dados não exista ele cria

--- MysqlConnection.php
+++ MysqlConnection.php
@@ -1,25 +1,39 @@
 <?php
+
 $pdo = new PDO("mysql:host=localhost;dbname=rato", "root", "");
 
-$sql = "CREATE DATABASE IF NOT EXISTS rato, CREATE TABLE IF NOT EXISTS clientes (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  rg VARCHAR(20) NOT NULL,
-  cpf VARCHAR(14) NOT NULL,
-  senha VARCHAR(50) NOT NULL
-)";
-if ($pdo->query($sql) === TRUE) {
-  echo "Banco de dados rato criado com sucesso!<br>";
+$sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'rato'";
+$resultado = $pdo->query($sql)->fetch();
+
+if(empty($resultado)){
+    $sql = "CREATE DATABASE rato";
+    $pdo->exec($sql);
+    echo "Banco de dados criado com sucesso!";
 } else {
-      echo "Erro ao criar banco de dados: " . $pdo->errorInfo()[2];
+    echo "O banco de dados já existe.";
 }
+
+$resultado = $pdo->query("SHOW TABLES LIKE 'clientes'")->fetch();
+if (!$resultado) {
+    $sql = "CREATE TABLE clientes (
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(50) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        rg VARCHAR(20) NOT NULL,
+        cpf VARCHAR(20) NOT NULL,
+        senha VARCHAR(50) NOT NULL
+    )";
+    $pdo->exec($sql);
+    echo "Tabela criada com sucesso!";
+} else {
+    echo "A tabela já existe.";
+}
+
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $rg = $_POST['rg'];
 $cpf = $_POST['cpf'];
 $senha = $_POST['senha'];
-
 
 $stmt = $pdo->prepare("INSERT INTO clientes (nome, email, rg, cpf, senha) VALUES (?, ?, ?, ?, ?)");
 $stmt->bindValue(1, $nome);
@@ -31,25 +45,26 @@ $stmt->execute();
 
 echo "Cadastro feito com sucesso!<br>";
 
-if (isset($_POST['login'])) {
-  $emailLogin = $_POST['email'];
-  $senha = $_POST['senha'];
+//tentativa de login
+// if (isset($_POST['login'])) {
+//   $emailLogin = $_POST['email'];
+//   $senha = $_POST['senha'];
 
-  $stmt = $pdo->prepare("SELECT * FROM clientes WHERE email = :email AND senha = :senha");
-  $stmt->bindParam(':email', $emailLogin);
-  $stmt->bindParam(':senha', $senha);
-  $stmt->execute();
+//   $stmt = $pdo->prepare("SELECT * FROM clientes WHERE email = :email AND senha = :senha");
+//   $stmt->bindParam(':email', $emailLogin);
+//   $stmt->bindParam(':senha', $senha);
+//   $stmt->execute();
 
-  if ($stmt->rowCount() > 0) {
-    $linha = $stmt->fetch(PDO::FETCH_ASSOC);
-    $_SESSION["Email"] = $linha['email'];
-    $_SESSION["Senha"] = $linha['senha'];
-    header("Location:cliente.php");
-  } else {
-    echo '<script type="text/javascript">';
-    echo 'alert("Email ou senha inválidos!!!");';
-    echo 'window.location.href="login.php"';
-  }
-}
+//   if ($stmt->rowCount() > 0) {
+//     $linha = $stmt->fetch(PDO::FETCH_ASSOC);
+//     $_SESSION["Email"] = $linha['email'];
+//     $_SESSION["Senha"] = $linha['senha'];
+//     header("Location:cliente.php");
+//   } else {
+//     echo '<script type="text/javascript">';
+//     echo 'alert("Email ou senha inválidos!!!");';
+//     echo 'window.location.href="login.php"';
+//   }
+// }
 ?>
 
