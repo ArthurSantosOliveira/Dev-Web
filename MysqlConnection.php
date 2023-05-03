e9bfdb75743f8749d0881a0d43fd4a35fb7a6684
ArthurSantosOliveira <pessoalarthursoliveira@gmail.com>
2023-05-03 09:46:08 -0300

    surtei
    login com php não esta funcionando ainda

--- MysqlConnection.php
+++ MysqlConnection.php
@@ -46,6 +46,26 @@ if (!$pdo) {
     echo "Cadastro feito com sucesso";
 
 }
+if(isset($_POST['login'])){
+    $emailLogin = $_POST['email'];
+    $senha = $_POST['senha'];
 
+    $stmt = $pdo->prepare("SELECT * FROM clientes WHERE email = :email AND senha = :senha");
+    $stmt->bindParam(':email', $emailLogin);
+    $stmt->bindParam(':senha', $senha);
+    $stmt->execute();
+
+    if($stmt->rowCount() > 0){
+        $linha = $stmt->fetch(PDO::FETCH_ASSOC);
+        $_SESSION["Email"] = $linha['email'];
+        $_SESSION["Senha"] = $linha['senha'];
+        header("Location:cliente.php");
+    }else{
+        echo'<script type = "text/javascript">';
+        echo'alert("Email ou senha inválidos!!!);';
+        echo'window.location.href="login.php"';
+        echo'</script>';
+    }
+}
 
 ?>
---
+++ login.php
@@ -0,0 +1,44 @@
+<?php
+    //  if(isset($_POST['login'])){
+    //     $emailLogin = $_POST['email'];
+    //     $senha = $_POST['senha'];
+        
+    //     $select = query($pdo,"SELECT * FROM clientes WHERE email = '$emailLogin' AND senha = '$senha'");
+    //     $linha = fetch_array($select);
+    //     if(is_array($linha)){
+    //         $_SESSION["Email"]=$linha['email'];
+    //         $_SESSION["Senha"]=$linha['senha'];
+    //     }else{
+    //         echo'<script type = "text/javascript">';
+    //         echo'alert("Email ou senha inválidos!!!);';
+    //         echo'window.location.href="login.php"';
+    //         echo'</script>';
+    //     }
+    //  }if(isset($_SESSION["Email"])){
+    //     header("Location:cliente.php");
+    //  }
+    $pdo = new PDO("mysql:host=localhost;dbname=rato", "root", ""); 
+
+    if(isset($_POST['login'])){
+        $emailLogin = $_POST['email'];
+        $senha = $_POST['senha'];
+    
+        $stmt = $pdo->prepare("SELECT * FROM clientes WHERE email = :email AND senha = :senha");
+        $stmt->bindParam(':email', $emailLogin);
+        $stmt->bindParam(':senha', $senha);
+        $stmt->execute();
+    
+        if($stmt->rowCount() > 0){
+            $linha = $stmt->fetch(PDO::FETCH_ASSOC);
+            $_SESSION["Email"] = $linha['email'];
+            $_SESSION["Senha"] = $linha['senha'];
+            header("Location:cliente.php");
+        }else{
+            echo'<script type = "text/javascript">';
+            echo'alert("Email ou senha inválidos!!!);';
+            echo'window.location.href="login.php"';
+            echo'</script>';
+        }
+    }
+    
+?>
