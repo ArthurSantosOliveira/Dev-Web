b40f931500e74fb62213c3b67fc6dfe1e1e61a34
ArthurSantosOliveira <pessoalarthursoliveira@gmail.com>
2023-05-03 13:45:36 -0300

    cirando banco caso não exista
    Co-Authored-By: Estheer2 <113109316+Estheer2@users.noreply.github.com>
    Co-Authored-By: Luiz Felipe Barreto Silveira  <112597491+Woouis@users.noreply.github.com>
    Co-Authored-By: Maurício Moura da Veiga <112085834+MauricioVeiga@users.noreply.github.com>

--- MysqlConnection.php
+++ MysqlConnection.php
@@ -1,71 +1,67 @@
 <?php
-// echo  'OK! <br>';
-// Seleciona o banco de dados desejado
-// $pdo->exec("USE rato");
+session_start();
 
-// Executa a query desejada
-// $query = "SELECT * FROM clientes"; 
-// $result_query = $pdo->query($query);
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$rg = $_POST['rg'];
+$cpf = $_POST['cpf'];
+$senha = $_POST['senha'];
 
-// Exibe os registros na tela
-// while ($row = $result_query->fetch(PDO::FETCH_ASSOC)) { 
-//     echo $row['nome'] , "<br>" ,$row['senha'] ."<br>"; 
-// }
+$pdo = new PDO("mysql:host=localhost;dbname=rato", "root", "");
 
-$nome =$_POST['nome'];
-$email =$_POST['email'];
-$rg =$_POST['rg'];
-$cpf =$_POST['cpf'];
-// $data_nascimento =$_POST['data_nascimento'];
-$senha =$_POST['senha'];
+$sql = "CREATE DATABASE IF NOT EXISTS rato";
 
-
-//conexao feita com sucesso
-// $pdo = new PDO("mysql:host=localhost;dbname=rato", "root", ""); 
-// if (!$pdo) {
-//     die('Erro ao conectar ao banco: ' . mysql_error());
-// }else{
-//     $stmt = $pdo->prepare("INSERT INTO clientes (nome, email, rg, cpf, dataNascimento,senha) values(?,?,?,?,?,?)");
-//     $stmt->bindValue($nome,$email,$rg,$cpf,$dataNascimento,$senha);
-//     $stmt->execute();
-//     echo "Cadastro feito com sucesso";
-//     $stmt-> close();
-//     $conn -> close();
-// }
-$pdo = new PDO("mysql:host=localhost;dbname=rato", "root", ""); 
-if (!$pdo) {
-    die('Erro ao conectar ao banco: ' . mysql_error());
+if ($pdo->query($sql) === TRUE) {
+  echo "Banco de dados rato criado com sucesso!<br>";
 } else {
-    $stmt = $pdo->prepare("INSERT INTO clientes (nome, email, rg, cpf, senha) values(?,?,?,?,?)");
-    $stmt->bindValue(1, $nome);
-    $stmt->bindValue(2, $email);
-    $stmt->bindValue(3, $rg);
-    $stmt->bindValue(4, $cpf);
-    $stmt->bindValue(5, $senha);
-    $stmt->execute();
-    echo "Cadastro feito com sucesso";
-
-}
-if(isset($_POST['login'])){
-    $emailLogin = $_POST['email'];
-    $senha = $_POST['senha'];
-
-    $stmt = $pdo->prepare("SELECT * FROM clientes WHERE email = :email AND senha = :senha");
-    $stmt->bindParam(':email', $emailLogin);
-    $stmt->bindParam(':senha', $senha);
-    $stmt->execute();
-
-    if($stmt->rowCount() > 0){
-        $linha = $stmt->fetch(PDO::FETCH_ASSOC);
-        $_SESSION["Email"] = $linha['email'];
-        $_SESSION["Senha"] = $linha['senha'];
-        header("Location:cliente.php");
-    }else{
-        echo'<script type = "text/javascript">';
-        echo'alert("Email ou senha inválidos!!!);';
-        echo'window.location.href="login.php"';
-        echo'</script>';
-    }
+  echo "Erro ao criar banco de dados: " . $pdo->errorInfo()[2];
 }
 
+
+$sql = "CREATE TABLE IF NOT EXISTS clientes (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  rg VARCHAR(20) NOT NULL,
+  cpf VARCHAR(14) NOT NULL,
+  senha VARCHAR(50) NOT NULL
+)";
+
+if ($pdo->query($sql) === TRUE) {
+  echo "Tabela clientes criada com sucesso!<br>";
+} else {
+  echo "Erro ao criar tabela: " . $pdo->errorInfo()[2];
+}
+
+$stmt = $pdo->prepare("INSERT INTO clientes (nome, email, rg, cpf, senha) VALUES (?, ?, ?, ?, ?)");
+$stmt->bindValue(1, $nome);
+$stmt->bindValue(2, $email);
+$stmt->bindValue(3, $rg);
+$stmt->bindValue(4, $cpf);
+$stmt->bindValue(5, $senha);
+$stmt->execute();
+
+echo "Cadastro feito com sucesso!<br>";
+
+if (isset($_POST['login'])) {
+  $emailLogin = $_POST['email'];
+  $senha = $_POST['senha'];
+
+  $stmt = $pdo->prepare("SELECT * FROM clientes WHERE email = :email AND senha = :senha");
+  $stmt->bindParam(':email', $emailLogin);
+  $stmt->bindParam(':senha', $senha);
+  $stmt->execute();
+
+  if ($stmt->rowCount() > 0) {
+    $linha = $stmt->fetch(PDO::FETCH_ASSOC);
+    $_SESSION["Email"] = $linha['email'];
+    $_SESSION["Senha"] = $linha['senha'];
+    header("Location:cliente.php");
+  } else {
+    echo '<script type="text/javascript">';
+    echo 'alert("Email ou senha inválidos!!!");';
+    echo 'window.location.href="login.php"';
+  }
+}
 ?>
+
