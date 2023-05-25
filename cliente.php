f0e927b141f66f3d67a2d941858f688e19d29c3b
ArthurSantosOliveira <pessoalarthursoliveira@gmail.com>
2023-05-25 14:25:04 -0300

    logout
    Co-Authored-By: Maurício Moura da Veiga <112085834+MauricioVeiga@users.noreply.github.com>
    Co-Authored-By: Luis Felipe Tavares <132400663+Tavaresreistk@users.noreply.github.com>

--- cliente.php
+++ cliente.php
@@ -5,7 +5,19 @@ session_start();
 // Se o usuário estiver logado, exibe a página
 include('conexao.php');
 
-echo 'Viadao' ;
-
-
+echo 'Bem vindo ',$_SESSION['Email'];
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Clique aqui para sair da sua conta </h1><a href="logout.php">Logout</a>
+</body>
+</html>
+
+
