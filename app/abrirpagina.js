5a7cfe4999e333c5a758c949055067a27cfc1318
ArthurSantosOliveira <pessoalarthursoliveira@gmail.com>
2023-06-05 18:15:14 -0300

    erro diretorio

--- app/abrirpagina.js
+++ app/abrirpagina.js
@@ -1,23 +1,23 @@
 function abrirPagina() {
-    window.location.href = "/html/camisa.html";
+  window.location.href = "../html/camisa.html";
 }
 
 function abrirPagina2() {
-    window.location.href = "/html/garrafa.html";
+  window.location.href = "../html/garrafa.html";
 }
 
 function abrirPagina3() {
-  window.location.href = "/html/capacete.html";
+window.location.href = "../html/capacete.html";
 }
 
 function abrirPagina4() {
-  window.location.href = "/html/camisaciclismo.html";
+window.location.href = "../html/camisaciclismo.html";
 }
 
 function abrirPagina5() {
-  window.location.href = "/html/oculos.html";
+window.location.href = "../html/oculos.html";
 }
 
 function abrirPagina6() {
-  window.location.href = "/html/suporte.html";
+window.location.href = "../html/suporte.html";
 }
