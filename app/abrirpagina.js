781817d2c330d73ba6541b3a8d232cad99524e19
Maurício Moura da Veiga <mauricioveiga27@gmail.com>
2023-06-06 20:04:13 -0300

    acessorios
    Co-Authored-By: Luiz Felipe Barreto Silveira  <112597491+Woouis@users.noreply.github.com>

--- app/abrirpagina.js
+++ app/abrirpagina.js
@@ -21,3 +21,23 @@ window.location.href = "../html/oculos.html";
 function abrirPagina6() {
 window.location.href = "../html/suporte.html";
 }
+
+function abrirPagina7(){
+  window.location.href = "../html/suportesqueeze.html";
+}
+
+function abrirPagina8(){
+  window.location.href = "../html/lanterna.html";
+}
+
+function abrirPagina9(){
+  window.location.href = "../html/velocimetro.html";
+}
+
+function abrirPagina10(){
+  window.location.href = "../html/cadeados.html";
+}
+
+function abrirPagina12(){
+  window.location.href = "../html/bolsabike.html";
+}
