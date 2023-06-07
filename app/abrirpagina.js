8b088436983de3a079de70097836cca3366dad5d
Maurício Moura da Veiga <mauricioveiga27@gmail.com>
2023-06-07 17:08:35 -0300

    Finalização das pags dos items acessorios
    Acabei todas as páginas para cada item dos acessórios e juntei os 2 javascript para o carrinho e pra abrir as paginas

--- app/abrirpagina.js
+++ app/abrirpagina.js
@@ -1,3 +1,17 @@
+function AddCarrinho(produto, qtd, valor, posicao)
+      {
+        localStorage.setItem("produto" + posicao, produto);
+        localStorage.setItem("qtd" + posicao, qtd);
+        valor = valor * qtd;
+        localStorage.setItem("valor" + posicao, valor);
+        alert("Produto adicionado ao carrinho!");
+      }
+
+      
+function fazerpedido()
+      {
+        alert("Pedido realizado com sucesso!");
+      }
 function abrirPagina() {
   window.location.href = "../html/camisa.html";
 }
@@ -37,9 +51,40 @@ function abrirPagina9(){
 function abrirPagina10(){
   window.location.href = "../html/cadeados.html";
 }
+
 function abrirPagina11(){
   window.location.href = "../html/bolsabike.html";
 }
+
 function abrirPagina12(){
   window.location.href = "../html/bombadear.html";
 }
+
+function abrirPagina13(){
+  window.location.href = "../html/paralamas.html";
+}
+
+function abrirPagina14(){
+  window.location.href = "../html/bolsaagua.html";
+}
+
+function abrirPagina15(){
+  window.location.href = "../html/bolsacelular.html";
+}
+
+function abrirPagina16(){
+  window.location.href = "../html/ferramentas.html";
+}
+
+function abrirPagina17(){
+  window.location.href = "../html/short.html";
+}
+
+function abrirPagina18(){
+  window.location.href = "../html/descanso.html";
+}
+
+
+
+
+
